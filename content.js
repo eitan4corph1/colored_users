1dee12bff8fa6f6a422f5d775a6d1e3e3158c105
ef239 <you@example.com>
2023-03-18 19:12:01 +0200

    color main table name

--- content.js
+++ content.js
@@ -2,17 +2,7 @@ const article = document.querySelector("a");
 
 console.log("start");
 
-let inputs = document.getElementsByTagName("a");
 
-console.log("a tags: ", inputs.length);
-
-for (let x = 0; x < inputs.length; x++) {
-    let myname = inputs[x].getAttribute("name");
-    if (myname && !isNaN(myname)) {
-        console.log("name: ", myname);
-        inputs[x].style.color = "red"
-    }
-}
 
 let tables = document.getElementsByTagName("TABLE");
 
@@ -28,8 +18,24 @@ for (let x = 0; x < tables.length; x++) {
                 user_id = td[td.length - 1].textContent;
                 user_name = td[td.length - 3].textContent;
                 console.log("id/user: ", user_id, ": ", user_name);
+                let font = td[td.length - 3].getElementsByTagName("font");
+                font[0].style.color = "red";
             }
         }
+    }
+}
+
+
+let inputs = document.getElementsByTagName("a");
+
+console.log("a tags: ", inputs.length);
+
+for (let x = 0; x < inputs.length; x++) {
+    let myname = inputs[x].getAttribute("name");
+    if (myname && !isNaN(myname)) {
+        let user = inputs[x].textContent;
+        console.log("user name messgae: ", user);
+        inputs[x].style.color = "red";
     }
 }
 
